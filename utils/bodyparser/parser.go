1df919584c56b90cf327651462ebfae856d26080
Denis Chernov <d3lusion4l.nom4d@gmail.com>
2026-08-06 16:42:07 +0300

    Cleaned up legacy bodyParser constants

--- utils/bodyparser/parser.go
+++ utils/bodyparser/parser.go
@@ -1,20 +1,14 @@
 package bodyparser
 
 import (
-	"github.com/imcrazytwkr/formdrain/constants"
-	m "github.com/imcrazytwkr/formdrain/models/http"
+	"github.com/imcrazytwkr/formdrain/models/http"
 )
 
 type BodyParser interface {
 	Parse(body []byte) (map[string]any, error)
 }
 
-var Parsers = map[string]BodyParser{
-	constants.ContentTypeForm: NewFormParser(),
-	constants.ContentTypeJson: NewJsonParser(),
-}
-
-var ParsersNew = map[m.ContentType]BodyParser{
-	m.ContentTypeHTML: NewFormParser(),
-	m.ContentTypeJSON: NewJsonParser(),
+var Parsers = map[http.ContentType]BodyParser{
+	http.ContentTypeHTML: NewFormParser(),
+	http.ContentTypeJSON: NewJsonParser(),
 }
