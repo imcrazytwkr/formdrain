a5971e1981dbaabd15f6493446ed3eec9cf02720
Denis Chernov <d3lusion4l.nom4d@gmail.com>
2026-08-06 17:01:48 +0300

    Cleaned-up form route constants

--- routes/form/logging.go
+++ routes/form/logging.go
@@ -6,6 +6,8 @@ import (
 	"github.com/rs/zerolog"
 )
 
+const actionSend = "send"
+
 func getLoggerForAction(ctx context.Context, action string) zerolog.Logger {
 	return zerolog.Ctx(ctx).With().Str("controller", "form").Str("action", action).Logger()
 }
