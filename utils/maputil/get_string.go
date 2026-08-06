b3b50034f60168df26638f5e24bbb8e4fde5f921
Denis Chernov <d3lusion4l.nom4d@gmail.com>
2026-08-06 16:39:59 +0300

    Updated string getter logic

--- utils/maputil/get_string.go
+++ utils/maputil/get_string.go
@@ -1,11 +1,13 @@
 package maputil
 
-func GetString[K comparable](obj map[K]any, key K) (string, bool) {
-	raw, ok := obj[key]
+import "strings"
+
+func GetString[K comparable](m map[K]any, key K) (string, bool) {
+	v, ok := m[key]
 	if !ok {
 		return "", ok
 	}
 
-	str, ok := raw.(string)
-	return str, ok
+	s, ok := v.(string)
+	return strings.TrimSpace(s), ok
 }
